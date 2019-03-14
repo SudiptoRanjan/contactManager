9f29e9a31ac90b3b3e27ae2797dd3737a8b77b41
sudipto ranjan <sudiptoranjan123@gmail.com>
2019-03-14 12:24:25 +0600

    Updates

---
+++ precache-manifest.85aef00d1dc6658767f3624fd8c099f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3162d77fe71abc74acf",
+    "url": "/contactManager/static/js/runtime~main.d3162d77.js"
+  },
+  {
+    "revision": "6b852d70ffe65bc14a6d",
+    "url": "/contactManager/static/js/main.6b852d70.chunk.js"
+  },
+  {
+    "revision": "54728a56bb1967fef6fc",
+    "url": "/contactManager/static/js/2.54728a56.chunk.js"
+  },
+  {
+    "revision": "6b852d70ffe65bc14a6d",
+    "url": "/contactManager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "54728a56bb1967fef6fc",
+    "url": "/contactManager/static/css/2.ac364ed7.chunk.css"
+  },
+  {
+    "revision": "3b7f92971582859f38e5c5bed0d8ab3f",
+    "url": "/contactManager/index.html"
+  }
+];
